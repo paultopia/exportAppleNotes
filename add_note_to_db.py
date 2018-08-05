83d05dc8a1bbb10295a2ec79e8b6ebb7d802101e
Paul Gowder <paul.gowder@gmail.com>
2018-08-04 21:23:19 -0500

    implement database dump

--- add_note_to_db.py
+++ add_note_to_db.py
@@ -1,5 +1,7 @@
 import sys
 from bs4 import BeautifulSoup
+import sqlite3
+
 
 def extract_text(soup):
     if soup is None:
@@ -12,7 +14,29 @@ def extract_text(soup):
     text = '\n\n'.join(chunk for chunk in chunks if chunk)
     return text
 
-text = extract_text(BeautifulSoup(sys.argv[1], "html.parser")) + "\n\n"
 
-with open("text.txt", 'a') as tt:
-    tt.write(text)
+def create_database(filename):
+    conn = sqlite3.connect(filename)
+    contracts_table = """ CREATE TABLE IF NOT EXISTS notes (
+                                        id integer PRIMARY KEY,
+                                        note text NOT NULL
+                                    ); """
+    c = conn.cursor()
+    c.execute(contracts_table)
+    return conn
+
+
+def add_data(conn, note):
+    insert_note = """ INSERT INTO notes(note)
+              VALUES(?) """
+    c = conn.cursor()
+    data = (note,)
+    c.execute(insert_note, data)
+    conn.commit()
+    return c.lastrowid
+
+
+if __name__ == "__main__":
+    text = extract_text(BeautifulSoup(sys.argv[1], "html.parser")) + "\n\n"
+    dbconn = create_database("notes.db")
+    add_data(dbconn, text)
